bc65350862c3a539a6c9d6107b7bec84d60cf7c5
Taui Silva <tauisilva@gmail.com>
2023-05-05 20:14:37 -0300

    preparação de ambiente
    configurado variáveis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MainComponent } from './main/main.component';
+import { MaterialModule } from 'src/assets/shared/material.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
