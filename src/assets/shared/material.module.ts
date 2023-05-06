a44fab2fd142137bb13c687cf7aec2e98ef0cb30
Taui Silva <tauisilva@gmail.com>
2023-05-05 21:25:25 -0300

    design de apresentação

--- src/assets/shared/material.module.ts
+++ src/assets/shared/material.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    MatButtonModule
+  ],
+  exports:[
+    MatCardModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
